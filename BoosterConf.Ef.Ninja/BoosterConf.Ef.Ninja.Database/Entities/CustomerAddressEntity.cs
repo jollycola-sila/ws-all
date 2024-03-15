ece8f83c120f5374b25b22da5768db54a1c64cf5
jollycola-sila <143261632+jollycola-sila@users.noreply.github.com>
2024-03-15 10:00:18 +0100

    Initialize checkpoint/task-d-done

---
+++ BoosterConf.Ef.Ninja/BoosterConf.Ef.Ninja.Database/Entities/CustomerAddressEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BoosterConf.Ef.Ninja.Database.Entities;
+
+[Table("CustomerAddresses", Schema = "pii")]
+public class CustomerAddressEntity
+{
+    public int Id { get; set; }
+    public Guid ExternalId { get; set; }
+
+    [MaxLength(length: 128)]
+    public required string Street { get; set; }
+
+    [MaxLength(length: 128)]
+    public required string City { get; set; }
+
+    [MaxLength(length: 128)]
+    public required string PostalCode { get; set; }
+
+    [MaxLength(length: 128)]
+    public required string Country { get; set; }
+}
